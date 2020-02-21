df61ae988f290c5ddebe54d7e4c6dabf702996d2
AlexK <alexjhon@live.com>
2020-02-20 23:27:25 -0500

    Edit padding

--- src/components/select.jsx
+++ src/components/select.jsx
@@ -9,15 +9,17 @@ for (let i = 1; i <= 10; i++) {
 
 const Select = ({ val, id, text, handleChange }) => (
   <div className="select">
-    <label htmlFor={id}>{text} </label>
-    <select
-      value={val}
-      onChange={e => handleChange(+e.target.value)}
-      name={id}
-      id={id}
-    >
-      {[...options]}
-    </select>
+    <center>
+      <label htmlFor={id}>{text} </label>
+      <select
+        value={val}
+        onChange={e => handleChange(+e.target.value)}
+        name={id}
+        id={id}
+      >
+        {[...options]}
+      </select>
+    </center>
   </div>
 );
 
